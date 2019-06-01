6ba89f9878ee6cd3b25c02982d54545b53c4509e
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-01 15:04:44 +0800

    rev

--- public/client.js
+++ public/client.js
@@ -58,8 +58,8 @@ function draw() {
         lvlCheckDelay=20;
         NbBumps++;
         document.getElementById("localBump").innerHTML = "Local Bumps = " + NbBumps.toString();
-        //socket.emit('shake',"shake"); // tell server the client mobile shakes
-        socket.broadcast.emit('shake',"shake");
+        socket.emit('shake',"shake"); // tell server the client mobile shakes
+        //socket.broadcast.emit('shake',"shake");
       }
       prevLevel = Level;
     }
