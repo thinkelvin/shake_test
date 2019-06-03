4966708822ed93a883932bf12228fbdfb0ff19e5
Kelvin Lee <thinkelvin@gmail.com>
2019-06-03 16:52:40 +0800

    a

--- public/client.js
+++ public/client.js
@@ -17,6 +17,9 @@ var XSpeed = 1; // shifting speed of x-axis
 var socket;
 // variables for track
 var track1Tap = false;
+var track2Tap = false;
+var track3Tap = false;
+var track4Tap = false;
 
 function preload() {
   //song = loadSound('song.mp3');
@@ -41,8 +44,14 @@ function setup() {
   socket.on('syncBump', syncBump);
   // set up touch responses
   var track1Element = document.getElementById('track1');
-  var mc = new Hammer(track1Element);
-  mc.on("tap", function(ev) {
+  var track2Element = document.getElementById('track2');
+  var track3Element = document.getElementById('track3');
+  var track4Element = document.getElementById('track4');
+  var mc1 = new Hammer(track1Element);
+  var mc2 = new Hammer(track2Element);
+  var mc3 = new Hammer(track3Element);
+  var mc4 = new Hammer(track4Element);
+  mc1.on("tap", function(ev) {
     track1Tap = !track1Tap;
     if (track1Tap) {
       track1Element.style.backgroundColor = "black";
@@ -50,8 +59,33 @@ function setup() {
     else {
       track1Element.style.backgroundColor =  "#ee0a0a";
     }
-
-    
+  });
+  mc2.on("tap", function(ev) {
+    track2Tap = !track2Tap;
+    if (track2Tap) {
+      track2Element.style.backgroundColor = "black";
+    }
+    else {
+      track2Element.style.backgroundColor =  "#25ee0a";
+    }
+  });
+  mc3.on("tap", function(ev) {
+    track3Tap = !track3Tap;
+    if (track3Tap) {
+      track3Element.style.backgroundColor = "black";
+    }
+    else {
+      track3Element.style.backgroundColor =  "#0accee";
+    }
+  });
+  mc4.on("tap", function(ev) {
+    track4Tap = !track4Tap;
+    if (track4Tap) {
+      track4Element.style.backgroundColor = "black";
+    }
+    else {
+      track4Element.style.backgroundColor =  "#e60aee";
+    }
   });
 }
 
