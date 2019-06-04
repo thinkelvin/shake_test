7ef0719e5f962f9a408118432c588a8fdc5ee7ba
Kelvin Lee <thinkelvin@gmail.com>
2019-06-04 15:12:10 +0800

    a

---
+++ public/accGraph.js
@@ -0,0 +1,48 @@
+
+
+function getLevel() {
+    return Level*30;
+  }
+  
+  function getaccX() {
+    //return accX;
+    return dAccX;
+  }
+  
+  Plotly.plot('accGraph', [
+    {
+      y: [getaccX()],
+      mode: 'lines',
+      type: 'line',
+      opacity: 0.5,
+      line: {
+        width: 2,
+        color: 'green'
+      }
+    },
+    {
+      y: [getLevel()],
+      mode: 'lines',
+      type: 'line'
+    }  
+  
+  ]);
+  
+  var cnt = 0;
+  var timePlot = 1000/FPS; // update the data at FPS
+  var displayRange = 200; // x-axis time range
+  setInterval(function() {
+    Plotly.extendTraces('accGraph', {
+      y: [
+        [getaccX()],[getLevel()]
+      ]
+    }, [0,1]); // normalize y datas into [0,1] range
+    cnt+=XSpeed;
+    //if (cnt > displayRange) {
+      Plotly.relayout('accGraph', {
+        xaxis: {
+          range: [cnt -displayRange , cnt+displayRange]
+        }
+      });
+    //}
+  }, 50); // millisecond to update
