e8d968bd476d8095840659e9b4f04e03568e4e04
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-04 22:00:35 +0800

    a

--- public/trackSoundSetup.js
+++ public/trackSoundSetup.js
@@ -44,7 +44,7 @@ function trackSoundSetup() {
             trackStarted = false;
             //track1End = false;track2End = false;track3End = false;track4End = false;
             //console.log('finised all tracks');
-        }
+        
     }
      
 }
