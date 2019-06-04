6d5c31685ef541ebb2239f64eae43c8b10523343
Kelvin Lee <thinkelvin@gmail.com>
2019-06-04 16:16:39 +0800

    a

--- public/trackSoundSetup.js
+++ public/trackSoundSetup.js
@@ -8,29 +8,36 @@ var track1Ready = false;
 var track2Ready = false;
 var track3Ready = false;
 var track4Ready = false;
-// var track1End = false;
-// var track2End = false;
-// var track3End = false;
-// var track4End = false;
-// var trackStarted = false;
+var track1End = false;
+var track2End = false;
+var track3End = false;
+var track4End = false;
+var trackStarted = false;
 
 function trackSoundSetup() {
+    if (!trackStarted) {
         if (track1Ready && track2Ready && track3Ready && track4Ready ){
           track1Sound.volume(0.5);track2Sound.volume(0.5);track3Sound.volume(0.5); track4Sound.volume(0.5);
           track1Sound.play(); track2Sound.play(); track3Sound.play();track4Sound.play();  
-          track1Ready = false; track2Ready = false; track3Ready = false; track4Ready = false;
+        //   track1Ready = false; track2Ready = false; track3Ready = false; track4Ready = false;
+          trackStarted = true;
           track1.on('end', function () {
-            track1Ready = true;
+            track1End = true;
           });
           track2.on('end', function () {
-            track2Ready = true;
+            track2End = true;
           });
           track3.on('end', function () {
-            track3Ready = true;
+            track3End = true;
           });
           track4.on('end', function () {
-            track4Ready = true;
+            track4End = true;
           });
       }
+    } else {
+        if (track1End && track2End && tack3End && track4End) {
+            trackStarted = false;
+        }
+    }
      
 }
