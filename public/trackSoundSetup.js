5339ee34c813742486b3a79ccf9ab48d818f8ea1
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-11 18:13:46 +0800

    a

--- public/trackSoundSetup.js
+++ public/trackSoundSetup.js
@@ -5,13 +5,21 @@ var trackStarted = true;
 var trackLoaded = 0;
 var trackEnded = 0;
 var trackIDs = [];
+var firstTimeSetup = true;
 
 function trackSoundSetup() {
-  if (!trackStarted && trackLoaded == 4) {
-    trackIDs[0] = trackSounds[0].play();
-    trackIDs[1] = trackSounds[1].play();
-    trackIDs[2] = trackSounds[2].play();
-    trackIDs[3] = trackSounds[3].play();
+  if (firstTimeSetup && trackLoaded==4) {
+        trackIDs[0] = trackSounds[0].play();
+        trackIDs[1] = trackSounds[1].play();
+        trackIDs[2] = trackSounds[2].play();
+        trackIDs[3] = trackSounds[3].play();
+        firstTimeSetup = false;
+  }
+  else if (!trackStarted && trackLoaded == 4) {
+    trackSounds[0].play(trackIDs[0]);
+    trackSounds[1].play(trackIDs[1]);
+    trackSounds[2].play(trackIDs[2]);
+    trackSounds[3].play(trackIDs[3]);
     trackStarted = true;
     trackSounds[0].on('end', function () {
       
