d922667d687701643c24078f2911a1153890da15
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-11 16:46:04 +0800

    a

--- public/trackSoundSetup.js
+++ public/trackSoundSetup.js
@@ -1,18 +1,6 @@
 // variables for track
 
-var track1Sound;
-var track2Sound;
-var track3Sound;
-var track4Sound;
 var trackSounds = [];
-var track1Ready = false;
-var track2Ready = false;
-var track3Ready = false;
-var track4Ready = false;
-var track1End = false;
-var track2End = false;
-var track3End = false;
-var track4End = false;
 var trackStarted = true;
 var trackLoaded = 0;
 var trackEnded = 0;
@@ -39,11 +27,9 @@ function trackSoundSetup() {
           });
       //}
     } else if (trackEnded == 4) {
-              //if (track1End && track2End && track3End && track4End) {
             trackEnded = 0;
             trackLoaded = 4;
             trackStarted = false;
-            //track1End = false;track2End = false;track3End = false;track4End = false;
             console.log('finised all tracks');
         
     }
