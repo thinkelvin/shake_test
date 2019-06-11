2f2c4f4b820f6ab3ac95e357c5a5c966aa7fdd8b
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-11 18:05:45 +0800

    a

--- public/trackSoundSetup.js
+++ public/trackSoundSetup.js
@@ -7,31 +7,36 @@ var trackEnded = 0;
 var trackIDs = [];
 
 function trackSoundSetup() {
-    if (!trackStarted && trackLoaded==4) {
-          trackIDs[0] = trackSounds[0].play();
-          trackIDs[1] = trackSounds[1].play();
-          trackIDs[2] = trackSounds[2].play();
-          trackIDs[3] = trackSounds[3].play();
-          trackStarted = true;
-          trackSounds[0].once('end', function () {
-            trackEnded++;
-          });
-          trackSounds[1].once('end', function () {
-            trackEnded++;
-          });
-          trackSounds[2].once('end', function () {
-            trackEnded++;
-          });
-          trackSounds[3].once('end', function () {
-            trackEnded++;
-          });
-      //}
-    } else if (trackEnded == 4) {
-            trackEnded = 0;
-            trackLoaded = 4;
-            trackStarted = false;
-            console.log('finised all tracks');
-        
-    }
+  if (!trackStarted && trackLoaded == 4) {
+    trackIDs[0] = trackSounds[0].play();
+    trackIDs[1] = trackSounds[1].play();
+    trackIDs[2] = trackSounds[2].play();
+    trackIDs[3] = trackSounds[3].play();
+    trackStarted = true;
+    trackSounds[0].on('end', function () {
+      
+      trackEnded++;
+    });
+    trackSounds[1].on('end', function () {
+      
+      trackEnded++;
+    });
+    trackSounds[2].on('end', function () {
      
+      trackEnded++;
+    });
+    trackSounds[3].on('end', function () {
+    
+      trackEnded++;
+    });
+    //}
+  } else if (trackEnded == 4) {
+    trackEnded = 0;
+    trackLoaded = 4;
+    trackStarted = false;
+
+    console.log('finised all tracks');
+
+  }
+
 }
