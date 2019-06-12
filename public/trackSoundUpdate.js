fbdf6ee5e304e0b196870f8c235efea657d9d2c5
film3056 <48443551+film3056@users.noreply.github.com>
2019-06-12 14:31:29 +0800

    a

--- public/trackSoundUpdate.js
+++ public/trackSoundUpdate.js
@@ -9,27 +9,24 @@ var firstLoop = true;
 var track1Sound, track2Sound, track3Sound, track4Sound;
 
 function trackSoundUpdate() {
-  // for (var i=0; i<4 ;i++){
-  //     trackSounds[i].mute(trackMuted[i]);
-  // }
   track1Sound.mute(trackMuted[0]);
   track2Sound.mute(trackMuted[1]);
   track3Sound.mute(trackMuted[2]);
   track4Sound.mute(trackMuted[3]);
 
-  if (firstLoop && !trackStarted && trackLoaded==4) {
-        trackIDs[0] = track1Sound.play();
-        trackIDs[1] = track2Sound.play();
-        trackIDs[2] = track3Sound.play();
-        trackIDs[3] = track4Sound.play();
-        firstLoop = false;
-  }
-  
-  if (!firstLoop && !trackStarted && trackLoaded == 4) {
-    track1Sound.play(trackIDs[0]);
-    track2Sound.play(trackIDs[1]);
-    track3Sound.play(trackIDs[2]);
-    track4Sound.play(trackIDs[3]);
+  if (!trackStarted && trackLoaded == 4) {
+    if (firstLoop) {
+      trackIDs[0] = track1Sound.play();
+      trackIDs[1] = track2Sound.play();
+      trackIDs[2] = track3Sound.play();
+      trackIDs[3] = track4Sound.play();
+      firstLoop = false;
+    } else {
+      track1Sound.play(trackIDs[0]);
+      track2Sound.play(trackIDs[1]);
+      track3Sound.play(trackIDs[2]);
+      track4Sound.play(trackIDs[3]);
+    }
     trackStarted = true;
     track1Sound.once('end', function () {
       trackEnded++;
@@ -43,14 +40,12 @@ function trackSoundUpdate() {
     track4Sound.once('end', function () {
       trackEnded++;
     });
-    
+
   } else if (trackEnded == 4) {
     trackEnded = 0;
     trackLoaded = 4;
     trackStarted = false;
-
     console.log('finised all tracks');
-
   }
 
 }
