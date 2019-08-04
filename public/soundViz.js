4f7392848c0684b476af36d1e5e49cdbecff13f4
film3056 <48443551+film3056@users.noreply.github.com>
2019-08-05 00:34:22 +0800

    aa

--- public/soundViz.js
+++ public/soundViz.js
@@ -6,7 +6,7 @@
             analyser1.getByteFrequencyData(dataArray1);
             let segWidth = bufferLength1 * 0.25;
             // var seg1 = dataArray1.slice(3 * segWidth, 4 * segWidth - 1);
-            var seg1 = dataArray1.slice(2 * segWidth +75 , 3 * segWidth - 1 + 75);
+            var seg1 = dataArray1.slice(100 , segWidth - 1);
             var seg2 = dataArray1.slice(2 * segWidth, 3 * segWidth - 1);
             var seg3 = dataArray1.slice(segWidth, 2 * segWidth - 1);
             var seg4 = dataArray1.slice(0, segWidth - 1);
@@ -15,7 +15,7 @@
             p2 = seg2.reduce(getSum) / segWidth;
             p3 = seg3.reduce(getSum) / segWidth;
             p4 = seg4.reduce(getSum) / segWidth;
-            p1 = p1 * 100 * 3 / 256;
+            p1 = p1 * 100 * 1.2 / 256;
             p2 = p2 * 100 * 1.5 / 256;
             p3 = p3 * 100 * 1.2 / 256;
             p4 = p4 * 100 * 1.3 / 256;
